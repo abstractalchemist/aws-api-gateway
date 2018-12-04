95c8affd652adbd52ac3f06e2a617c7c77d6d788
Jason Hirata <jhirata@integrity-apps.com>
2018-12-04 10:01:30 -1000

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,27 @@
 const gulp = require('gulp')
-const browersify = require('browserify')
+const browserify = require('browserify')
+const mocha = require('gulp-mocha')
+const babelify = require('babelify')
+const fs = require('fs')
 
-gulp.task('js', function() {
-   console.log('starting js');
+
+gulp.task('test', function() {
+   return gulp.src('test/**/*js').pipe(mocha({compilers:'js:@babel/register'}))
 })
 
-gulp.task('default', ['js'], function() {
-   return gulp.watch(['src/**/*js', 'test/**/*js'], ['js')
+gulp.task('js', ['test'], function() {
+   return browserify('src/index.js')
+      .transform('babelify')
+      .bundle()
+      .pipe(fs.createWriteStream('dist/bundle.js'))
+})
+
+
+gulp.task('cp', function() {
+   return gulp.src('public/*')
+      .pipe(gulp.dest('dist'))
+})
+
+gulp.task('default', ['js', 'cp'], function() {
+   return gulp.watch(['src/**/*js', 'test/**/*js'], ['js'])
 })
---
+++ test/index.js
@@ -0,0 +1,7 @@
+import { expect } from 'chai'
+
+describe('basic test', function() {
+   it('testing', function() {
+      expect(true).to.be.true
+   })
+})
